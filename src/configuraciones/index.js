311e02d1ba4273913ed0b1976fe05a63d97680c3
Franco <francisco.recalde.91@gmail.com>
2021-11-14 23:54:35 -0300

    environment

--- src/configuraciones/index.js
+++ src/configuraciones/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: "../../.env" })
 
 const env = {
-  server: process.env.REACT_APP_API_URL || "http://localhost:3030"
+  server: process.env.NODE_ENV === 'development' ? "http://localhost:3030" : process.env.REACT_APP_API_URL
 }
 
 export default env
